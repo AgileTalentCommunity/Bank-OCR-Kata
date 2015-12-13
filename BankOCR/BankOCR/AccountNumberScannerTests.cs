d9435e57c99e7e9994e0ed2ceb990f8a3fdb8503
Sergio <sergio.villen.galera@hotmail.fr>
2015-12-13 23:46:04 +0100

    Test refactoring

--- BankOCR/BankOCR/AccountNumberScannerTests.cs
+++ BankOCR/BankOCR/AccountNumberScannerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,51 +10,18 @@ using NUnit.Framework;
 namespace BankOCR
 {
     public class AccountNumberScannerTests
-    {
-
-        [Test]
-        public void Should_return_00_when_input_is_00()
+    {        
+        [TestCase("00")]
+        [TestCase("000000000")]
+        [TestCase("111111111")]
+        [TestCase("222222222")]
+        [TestCase("333333333")]
+        public void Should_return_000000000_when_input_is_000000000(string expected)
         {
             var scanner = new AccountNumberScanner();
-            var input = " _  _ \n| || |\n|_||_|\n      ";
+            var input = File.ReadAllText($"./TestInputs/{expected}.txt");            
             var actual = scanner.Scan(input);
-            Check.That(actual).IsEqualTo("00");
-        }
-
-        [Test]
-        public void Should_return_000000000_when_input_is_000000000()
-        {
-            var scanner = new AccountNumberScanner();
-            var input = " _  _  _  _  _  _  _  _  _ \n| || || || || || || || || |\n|_||_||_||_||_||_||_||_||_|\n                           ";
-            var actual = scanner.Scan(input);
-            Check.That(actual).IsEqualTo("000000000");
-        }
-
-        [Test]
-        public void Should_return_111111111_when_input_is_111111111()
-        {
-            var scanner = new AccountNumberScanner();
-            var input = "                           \n  |  |  |  |  |  |  |  |  |\n  |  |  |  |  |  |  |  |  |\n                           ";
-            var actual = scanner.Scan(input);
-            Check.That(actual).IsEqualTo("111111111");
-        }
-
-        [Test]
-        public void Should_return_222222222_when_input_is_222222222()
-        {
-            var scanner = new AccountNumberScanner();
-            var input = " _  _  _  _  _  _  _  _  _ \n _| _| _| _| _| _| _| _| _|\n|_ |_ |_ |_ |_ |_ |_ |_ |_ \n                           ";
-            var actual = scanner.Scan(input);
-            Check.That(actual).IsEqualTo("222222222");
-        }
-
-        [Test]
-        public void Should_return_333333333_when_input_is_333333333()
-        {
-            var scanner = new AccountNumberScanner();
-            var input = " _  _  _  _  _  _  _  _  _ \n _| _| _| _| _| _| _| _| _|\n _| _| _| _| _| _| _| _| _|\n                           ";
-            var actual = scanner.Scan(input);
-            Check.That(actual).IsEqualTo("333333333");
+            Check.That(actual).IsEqualTo(expected);
         }
     }
 }
